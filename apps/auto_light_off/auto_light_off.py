5606738e004075b0dd30b04e981483f646689385
appdaemon <krokko@gmail.com>
2023-07-26 14:43:46 +0200

    Working on mqtt app as a device, MQTT queues configuration

--- apps/auto_light_off/auto_light_off.py
+++ apps/auto_light_off/auto_light_off.py
@@ -18,23 +18,26 @@ class AutoLightOff(hass.Hass):
         self.Watched_devices = self.args["Watched_devices"]
         self.Excluded_night_devices = self.args["Excluded_night_devices"]
         self.AppDetails = self.args["AppDetails"]
-
+        self.Buttons = {}
+        self.Sensors = {}
+        self.Numbers = {}
+        self.Switches = {}
         for ent in self.Watched_devices:
             self.listen_state(self.byrna, ent)
             self.log(f"Adding {ent} to watch.")
 
         args = self.args
         # print (self.args)
-        states = self.get_state("switch")
         
-        for ent in states.values():
-            friendly_name_of_entity = ent.get("attributes").get("friendly_name")
+        for ent in self.Watched_devices:
+            states = self.get_entity(ent).get_state(attribute="all")
+            friendly_name_of_entity = states.get("attributes").get("friendly_name")
 #            friendly_name_of_entity = "ent"
-            ent_id = ent['entity_id']#.replace(".","xx")
+            ent_id = states.get("entity_id")#.replace(".","xx")
             ent_data = {"command_topic": f"{ent_id}",
                         "state_topic": f"{ent_id}/set",
                         "mode": "box",
-                        "unique_id": f"kurwa_{ent_id}_idd",
+                        "unique_id": f"{ent_id}",
                         "name": f"{friendly_name_of_entity}",
                         "friendly_name":"2137"
                         }
@@ -43,54 +46,76 @@ class AutoLightOff(hass.Hass):
             post_data.update({"dev":self.AppDetails})
             post_data.update(ent_data)
             print (json.dumps(post_data,indent=4))
-            tt = f"homeassistant/switch/{ent['entity_id']}/config".replace(".","")
+            tt = f"homeassistant/switch/{ent_id}/config".replace(".","")
+            self.Switches.update({ent_id:ent_data})
             print (tt)
             mqtt.mqtt_publish(self, topic=tt, payload=json.dumps(post_data))
 
-            ent_data = {"command_topic": f"{ent_id}",
-                        "state_topic": f"{ent_id}/set",
-                        "availability_topic": f"{ent_id}/av",
-                        "unique_id": f"kurfrrwa_{ent_id}_idd",
+            tt = f"homeassistant/number/kupa/{ent_id}/config".replace(".","xx")
+            ent_data = {"command_topic": f"{tt}",
+                        "state_topic": f"{tt}/set",
+                        "availability_topic": f"{tt}/av",
+                        "unique_id": f"{tt}",
                         "name": f"{friendly_name_of_entity}",
+                        "mode":"box"
                         # "friendly_name": "2137"
                         }
             post_data = {}
             post_data.update({"dev":self.AppDetails})
             post_data.update(ent_data)
             print (json.dumps(post_data,indent=4))
-            tt = f"homeassistant/number/kupa/{ent['entity_id']}/config".replace(".","xx")
+            self.Numbers.update({ent_id:ent_data})
             print (tt.format(typee="wefwwfew"))
             mqtt.mqtt_publish(self, topic=tt, payload=json.dumps(post_data))
+            mqtt.mqtt_publish(self, topic=ent_data.get("availability_topic"), payload="online")
+            mqtt.mqtt_publish(self, topic=ent_data.get("state_topic"), payload=43)
 
             ent_data = {"command_topic": f"{ent_id}",
                         "state_topic": f"{ent_id}/set",
                         "availability_topic": f"{ent_id}/av",
-                        "unique_id": f"kurfrrwa_{ent_id}_idd",
-                        "name": f"{friendly_name_of_entity}",
+                        "unique_id": f"dfvfdv{ent_id}",
+                        "name":f"{friendly_name_of_entity}"
                         # "friendly_name": "2137"
                         }
             post_data = {}
             post_data.update({"dev":self.AppDetails})
             post_data.update(ent_data)
             print (json.dumps(post_data,indent=4))
-            tt = f"homeassistant/sensor/Eeee{ent['entity_id']}".replace(".","xx")
+            tt = f"homeassistant/sensor/fvfvf{ent_id}".replace(".","xx")
+            self.Sensors.update({ent_id:ent_data})
             print (tt.format(typee="wefwwfew"))
             mqtt.mqtt_publish(self, topic=f"{tt}/config", payload=json.dumps(post_data))
+            mqtt.mqtt_publish(self, topic=ent_data.get("availability_topic"), payload="online")
 
             ent_data = {"command_topic": f"{tt}/cmnd",
                         "state_topic": f"{tt}/set",
                         "availability_topic": f"{tt}/av",
-                        "unique_id": f"kurfrrwa_{ent_id}_idd",
-                        "name": f"{friendly_name_of_entity}",
+                        "unique_id": f"{ent_id}",
+                        "name": f"{friendly_name_of_entity}"
                         # "friendly_name": "2137"
                         }
             post_data = {}
             post_data.update({"dev":self.AppDetails})
             post_data.update(ent_data)
             print (json.dumps(post_data,indent=4))
-            tt = f"homeassistant/button/Eeee{ent['entity_id']}/config".replace(".","xx")
+            tt = f"homeassistant/button/{ent_id}/config".replace(".","xx")
+            self.Buttons.update({ent_id: ent_data})
             print (tt.format(typee="wefwwfew"))
             mqtt.mqtt_publish(self, topic=tt, payload=json.dumps(post_data))
+            mqtt.mqtt_publish(self, topic=ent_data.get("availability_topic"), payload="online")
+
+            print(ent_data.get("availability_topic"))
+        # for button in self.Buttons.values():
+        #     print(button.get("availability_topic"))
+        #     mqtt.mqtt_publish(self, topic=button.get("availability_topic"), payload="online")
+        #
+        # for number in self.Numbers.values():
+        #
+        #     mqtt.mqtt_publish(self, topic=number.get("availability_topic"), payload="online")
+        # print(json.dumps(self.Sensors,indent=4))
+        # print(json.dumps(self.Buttons,indent=4))
+        # print(json.dumps(self.Numbers,indent=4))
+        # print(json.dumps(self.Switches,indent=4))
 
     def auto_byrna_off(self, args):
         self.log(f"runned in delay  ---{args['ent']}")
@@ -136,6 +161,9 @@ class AutoLightOff(hass.Hass):
                 try:
                     handler = self.run_in(self.auto_byrna_off, off_time, ent=entity)
                     self.handlers.update({entity: handler})
+                    mqtt.mqtt_publish(self,topic=self.Sensors.get(entity).get("availability_topic"),payload="online")
+                    timer, interval, kwargs = self.info_timer(self.handlers.get(entity))
+                    mqtt.mqtt_publish(self,self.Sensors.get(entity).get("state_topic"),payload=timer.strftime("%H:%M:%S"))
                     self.log(f"Adding to auto off schedule light {friendly_name_of_entity} in {off_time} Minutes")
                 except Exception as e:
                     self.log(f"An Exception occurred during turn of a lamp:"
@@ -146,12 +174,14 @@ class AutoLightOff(hass.Hass):
         if new == "off":
             if self.handlers.get(entity):
                 try:
+                    self.log("kurwirjaeirui")
                     self.cancel_timer(self.handlers[entity])
                     self.handlers.pop(entity)
+                    mqtt.mqtt_publish(self,self.Sensors.get(entity).get("state_topic"),payload="None")
                     self.log(f"Manual light off occurred, removing {entity} from off schedule")
                 except Exception as e:
-                    self.log(e)
-            self.log("Turned OFF", entity)
+                    self.log(f"Exception occured: {e}")
+            # self.log("Turned OFF", entity)
 
     def terminate(self):
         self.log("Terminating")
